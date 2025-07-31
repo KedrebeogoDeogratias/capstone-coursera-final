227ba29c7c330738751088ad8b8ef37ab6fef377
Kedrebeogo Deogratias <Kedrebeogodeogratias@aol.com>
2025-07-31 15:22:09 +0000

    settings

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './Css/App.css';
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div id="app">
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
