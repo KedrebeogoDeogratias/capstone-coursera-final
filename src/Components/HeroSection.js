65f9436786bfd8b30704664e6c5272cddc1fe7f3
Kedrebeogo Deogratias <Kedrebeogodeogratias@aol.com>
2025-07-31 16:27:47 +0000

    favicon add

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -10,8 +10,8 @@ function HeroSection() {
             <article id="hero-text">
                 <h1>Little Lemon</h1>
                 <h2>Chicago</h2>
-                <p className="lead-text">We are a family owned Mediterranean restaurant, focused on
-                 traditional recipes served with a modern twist.</p>
+                <p className="lead-text">Welcome to Little Lemon
+At Little Lemon, we bring bold Mediterranean flavors to the heart of your city. Our chefs blend fresh ingredients, traditional recipes, and modern twists to create unforgettable dishes in a warm and inviting atmosphere. Whether you're here for a casual lunch or a cozy dinner, experience the zest of good food and great company.</p>
                 <Link to="/booking" className="link-button"><button>Reserve a Table</button></Link>
             </article>
             <img src={foodPhoto} width="330" height="388" alt="restaurant food served on a plate"></img>
